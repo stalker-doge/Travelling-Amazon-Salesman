ca5a277ad28b18f81f12b692d2e41a5d1016678a
LucianMATAN <60778889+LucianMATAN@users.noreply.github.com>
2021-11-24 11:17:52 +0000

    Initial commit

---
+++ Travelling Amazon Salesman/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void main()
+{
+	string strVal, strLine;
+	ifstream ArrayInputFile;
+	ArrayInputFile.open("TSP10.csv", ios::in);
+	const int ROWS = 15;
+	const int COLS = 15;
+	int cityDistances[ROWS][COLS];
+	int startPos, pos;
+
+	for (int row = 0; row < ROWS; row++)
+	{
+		ArrayInputFile >> strLine;
+		startPos = 0;
+		pos = strLine.find(",");
+		for (int col = 0; col < COLS - 1; col++)
+		{
+			strVal = strLine.substr(startPos, pos - startPos);
+			cityDistances[row][col] = atoi(strVal.c_str());
+			startPos = pos + 1;
+			pos = strLine.find(",", startPos);
+		}
+		strVal = strLine.substr(startPos, strLine.length() - startPos);
+		cityDistances[row][COLS - 1] = atoi(strVal.c_str());
+	}
+	cout << "\nCity Distances:\n";
+	for (int row = 0; row < ROWS; row++)
+	{
+		for (int col = 0; col < COLS; col++)
+		{
+			cout << "\t" << cityDistances[row][col];
+		}
+		cout << "\n";
+	}
+	system("pause");
+}
