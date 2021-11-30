3e296da39f9016efe3cf7fe981083ea5fb18afcb
stalker-doge <94372701+stalker-doge@users.noreply.github.com>
2021-11-30 10:45:23 +0000

    done the primary marks
    calculates 1000 random paths and finds the best and worst path and distances

--- Travelling Amazon Salesman/Source.cpp
+++ Travelling Amazon Salesman/Source.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 void main()
 {
@@ -11,13 +13,20 @@ void main()
 	const int ROWS = 15;
 	const int COLS = 15;
 	int cityDistances[ROWS][COLS];
-	int tspPath[16];
+	int tspPath[16],smallestTspPath[16];
 	tspPath[0] = 0;
 	tspPath[15] = 0;
+	smallestTspPath[0] = 0;
+	smallestTspPath[0] = 0;
+	int totalDist = 0;
+	int smallestTotalDist = 9999999;
+	int biggestTotalDist = 0;
+	float percentageDifferencePaths;
 	srand(time(NULL));
 	int rndCity = rand() % 14 + 1;
 	bool alreadyChosen = true;
 	int startPos, pos;
+
 	for (int row = 0; row < ROWS; row++)
 	{
 		ArrayInputFile >> strLine;
@@ -33,30 +42,59 @@ void main()
 		strVal = strLine.substr(startPos, strLine.length() - startPos);
 		cityDistances[row][COLS - 1] = atoi(strVal.c_str());
 	}
-	for (int currPos = 1; currPos < 15; currPos++)
+	auto start = high_resolution_clock::now();
+	for (int loop = 0; loop < 1000; loop++)
 	{
-		alreadyChosen = true;
-		do
+		for (int currPos = 1; currPos < 15; currPos++)
 		{
-			rndCity = rand() % 14 + 1;
-			alreadyChosen = false;
-
-			for (int pos = 1; pos < currPos; pos++)
+			alreadyChosen = true;
+			do
 			{
-				if (tspPath[pos] == rndCity)
-				{
-					alreadyChosen = true;
-				}
-			}
+				rndCity = rand() % 14 + 1;
+				alreadyChosen = false;
 
-		} while (alreadyChosen == true);
-		tspPath[currPos] = rndCity;
+				for (int pos = 1; pos < currPos; pos++)
+				{
+					if (tspPath[pos] == rndCity)
+					{
+						alreadyChosen = true;
+					}
+				}
+
+			} while (alreadyChosen == true);
+			tspPath[currPos] = rndCity;
+		}
+		for (int pos = 0; pos < 15; pos++)
+		{
+			totalDist = totalDist + cityDistances[tspPath[pos]][tspPath[pos + 1]];
+		}
+		if (smallestTotalDist>totalDist)
+		{
+			smallestTotalDist = totalDist;
+				for (int pos = 0; pos < 15; pos++)
+				{
+					smallestTspPath[pos] = tspPath[pos];
+				}
+		}
+		if (biggestTotalDist < totalDist)
+		{
+			biggestTotalDist = totalDist;
+		}
+		totalDist = 0;
+		cout << "\ntest\n";
 	}
-	cout << "\nCurrent path is:\n";
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	percentageDifferencePaths =((biggestTotalDist - smallestTotalDist) / ((biggestTotalDist + smallestTotalDist)*0.5))*100;
+	cout << "\nSmallest path is:\n";
 	for (int i = 0; i < 16; i++)
 	{
 		cout << "\t" << tspPath[i];
 	}
+	cout << "\nAnd the distance is :" << smallestTotalDist << "\n";
+	cout << "\nThe worst path is: " << biggestTotalDist << "\n";
+	cout << "\nPercentage difference between the two is: " << percentageDifferencePaths << "%\n";
+	cout << "\nIt took: " << duration.count() << "ms for the program to process 1000 random paths";
 	cout << "\n";
 	system("pause");
 }
